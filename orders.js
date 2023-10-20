7d164c996f58b8601ea074929571abab208c684c
KorpenChan <Korpenxd@gmail.com>
2023-10-20 14:39:13 +0200

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Gamer Feng Shui',
+        productNumber: '70315',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Gamer Full Course',
+        productNumber: '49385',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Epic Gamer Montage Tutorial',
+        productNumber: '35745',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
